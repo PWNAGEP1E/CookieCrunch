a33730bf3f4b34a57a66d526ef12f8d10cbd415b
PWNAGEP1E <jeremyleemednik@gmail.com>
2016-03-03 14:24:02 -0500

    Initial Commit

---
+++ Cookie CrunchTests/Cookie_CrunchTests.swift
@@ -0,0 +1,36 @@
+//
+//  Cookie_CrunchTests.swift
+//  Cookie CrunchTests
+//
+//  Created by jeremy on 3/3/16.
+//  Copyright © 2016 Indicane. All rights reserved.
+//
+
+import XCTest
+@testable import Cookie_Crunch
+
+class Cookie_CrunchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
